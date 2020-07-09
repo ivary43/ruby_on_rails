5207564c2e5ef00487b7e3b4759b3d8cf1948051
Manish Kumar <manish.kumar@elucidata.io>
2020-07-09 19:08:13 +0530

    new routes added

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -10,5 +10,40 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end	
 
+	def new 
+		@article = Article.new 
+	end 
+
+	def edit
+		@article = Article.find(
+			params[:id]
+			)
+	end 
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = 'Sucessfully saved'
+			redirect_to article_path(@article)
+		else 
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = 'Sucessfully updated'
+			redirect_to article_path(@article)
+		else 
+			render 'new'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end 
 end
 	
